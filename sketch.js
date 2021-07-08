128be2f4b5ed0926655d66227861950c546b18c8
TechnoKirb <87034229+TechnoKirb@users.noreply.github.com>
2021-07-08 14:57:25 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
-
+var small_body_of_water,air_car,water_image,plane_image
 function preload(){
-
+  water_image= loadImage('sea.png')
+  plane_image=loadAnimation('ship-1.png','ship-2.png','ship-3.png','ship-4.png')
 }
 
 function setup(){
   createCanvas(400,400);
-  
+  small_body_of_water=createSprite(300,300,400,400)
+  small_body_of_water.addImage(water_image)
+  small_body_of_water.velocityX=-1.5
+  air_car=createSprite(200,300,50,50)
+  air_car.addAnimation('I like trains',plane_image)
 }
 
 function draw() {
-  background("blue");
- 
+//  background("blue");
+  if(small_body_of_water.x<100){
+    small_body_of_water.x=small_body_of_water.width/2
+  }
 }
